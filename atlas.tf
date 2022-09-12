da22bae9e2172c206c3638eac80f9084e15b5fd2
Arjun Goel <arjungoel1995@gmail.com>
2022-09-12 12:08:58 +0530

    updated atlas code

--- atlas.tf
+++ atlas.tf
@@ -1,10 +1,10 @@
-resource "mongodbatlas_project" "elasticsi_test" {
+resource "mongodbatlas_project" "mongo_test" {
   name   = var.mongodbatlas_project_name
   org_id = var.mongodbatlas_orgid
 }
 
-resource "mongodbatlas_cluster" "elasticsi_cluster" {
-  project_id = mongodbatlas_project.elasticsi_test.id
+resource "mongodbatlas_cluster" "mongo_cluster" {
+  project_id = mongodbatlas_project.mongo_test.id
   name       = var.mongodbatlas_cluster
   # Provider Settings "block"
   provider_name               = "TENANT"
@@ -13,21 +13,21 @@ resource "mongodbatlas_cluster" "elasticsi_cluster" {
   provider_instance_size_name = "M0"
 }
 
-resource "mongodbatlas_database_user" "elasticsi_db_user" {
+resource "mongodbatlas_database_user" "mongo_db_user" {
   username           = var.mongodbatlas_db_username
   password           = var.mongodbatlas_db_password
-  project_id         = mongodbatlas_project.elasticsi_test.id
+  project_id         = mongodbatlas_project.mongo_test.id
   auth_database_name = "admin"
 
   roles {
     role_name     = "readWrite"
     database_name = "dbadmin"
   }
-  depends_on = [mongodbatlas_project.elasticsi_test]
+  depends_on = [mongodbatlas_project.mongo_test]
 }
 
-resource "mongodbatlas_network_container" "elasticsi_container" {
-  project_id       = mongodbatlas_project.elasticsi_test.id
+resource "mongodbatlas_network_container" "mongo_container" {
+  project_id       = mongodbatlas_project.mongo_test.id
   atlas_cidr_block = var.atlas_vpc_cidr_block
   provider_name    = "AWS"
   region_name      = "US_EAST_1"
@@ -35,8 +35,8 @@ resource "mongodbatlas_network_container" "elasticsi_container" {
 
 resource "mongodbatlas_network_peering" "test" {
   accepter_region_name   = var.aws_region
-  project_id             = mongodbatlas_project.elasticsi_test.id
-  container_id           = mongodbatlas_network_container.elasticsi_container.id
+  project_id             = mongodbatlas_project.mongo_test.id
+  container_id           = mongodbatlas_network_container.mongo_container.id
   provider_name          = "AWS"
   route_table_cidr_block = aws_vpc.main.cidr_block
   vpc_id                 = aws_vpc.main.id
@@ -44,7 +44,7 @@ resource "mongodbatlas_network_peering" "test" {
 }
 
 resource "mongodbatlas_project_ip_access_list" "test" {
-  project_id = mongodbatlas_project.elasticsi_test.id
+  project_id = mongodbatlas_project.mongo_test.id
   cidr_block = aws_vpc.main.cidr_block
   comment    = "cidr block for AWS VPC"
 }
